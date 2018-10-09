fd46a7f75b0da6c8f9a10585df95cde6ef7f38db
sbdales <shaundales@outlook.com>
2018-10-09 14:23:05 -0400

    Update Game.js

--- Game.js
+++ Game.js
@@ -1,7 +1,7 @@
 const GameState = Object.freeze({
     WELCOMING:   Symbol("welcoming"),
-    STICK:  Symbol("stick"),
-    PLAY: Symbol("play")
+    START:  Symbol("start"),
+    RUNNING: Symbol("running")
 });
 
 export default class Game{
@@ -11,25 +11,29 @@ export default class Game{
     
     makeAMove(sInput)
     {
-        let sReply = "Hi I'm spot (a dog). Oh look a stick. Do you play or keep on walking?";
+        let sReply = "You stand outside an old delapidated house.  Your friends are daring you to go inside, do you ENTER or RUN?";
         switch(this.stateCur){
             case GameState.WELCOMING:
-                this.stateCur = GameState.STICK;
+                this.stateCur = GameState.START;
                 break;
-            case GameState.STICK:
-                if(sInput.toLowerCase().match("play")){
-                    sReply = "Great my favourite game ... Here's the stick back. Do you throw it again?"
-                    this.stateCur = GameState.PLAY;
+            case GameState.START:
+                if(sInput.toLowerCase().match("run")){
+                    sReply = "You pee your pants and run away crying.  Your friends are laughing at you.  Do you GO back or KEEP running?"
+                    this.stateCur = GameState.RUNNING;
+                }else if(sInput.toLowerCase().match("enter")){
+                    sReply = "You open the door and enter.  The inside of the house is dark and it's hard to see.  You can make out a room to the LEFT, a HALL ahead of you and STAIRS to the right. Which way do you go??";
                 }else{
-                    sReply = "Walking is my favourite. Oh look a stick! Do you play or keep on walking?";
+                    sReply = "You stand there paralized with fear.  Do you RUN or ENTER?";
                 }
+                
                 break;
-            case GameState.PLAY:
-                if(sInput.toLowerCase().match("yes")){
-                    sReply = "Here it is! I got it for you. Do you toss it again.... Please? ";
-                }else{
-                    sReply = "Walking is my favourite. Oh look a stick! Do you play or keep on walking?";
-                    this.stateCur = GameState.STICK;
+            case GameState.RUNNING:
+                if(sInput.toLowerCase().match("go")){
+                    sReply = "You have rejoined your friends and the still are daring you to enter the house, do you ENTER or RUN? ";
+                    this.stateCur = GameState.START;
+                }else {
+                    sReply = "You keep running until you are home safe with your mommy.  She tells you to go back and find your friends";
+                    this.stateCur = GameState.WELCOMING;
                 }
                 break;
         }
